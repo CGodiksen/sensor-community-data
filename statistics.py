bf7ce3cf8312f5d30716243e1a84b5cbedc760de
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-02-09 21:52:03 +0100

    Added function to get time frames of dateframes.

--- statistics.py
+++ statistics.py
@@ -25,6 +25,16 @@ class Statistics:
 
     @staticmethod
     def __get_time_frame(dataframes):
+        earliest_date = None
+        latest_date = None
+
+        for df in dataframes:
+            date = df.at[0, "timestamp"][:10]
+
+            if date < earliest_date:
+                earliest_date = date
+            elif date > latest_date:
+                latest_date = date
 
     @staticmethod
     def __get_city_statistics(dataframes):
