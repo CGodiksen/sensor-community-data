734cfcf87e1892126960842592e86e411d3e090a
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-02-23 12:49:16 +0100

    Data cleaning testing.

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,14 +1,23 @@
 import pandas as pd
+from hampel import hampel
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 class DataVisualizer:
     @staticmethod
-    def get_line_chart(file_path):
-        series = pd.read_csv(file_path, header=0, index_col=0, parse_dates=True, squeeze=True)
-        series.plot()
+    def get_line_chart(folder_path):
+        dataframes = []
+        for csv_file in Path(folder_path).rglob("*.csv"):
+            dataframes.append(pd.read_csv(csv_file, header=0, index_col=0, parse_dates=True, squeeze=True))
+
+        series = pd.concat(dataframes)
+        series_1 = series[["P1"]]
+
+        series_cleaned = hampel(series_1.squeeze(), window_size=20, n=3)
+        series_cleaned.plot()
         plt.show()
 
 
 test = DataVisualizer()
-test.get_line_chart("data/1613467525_preprocessed/Stuttgart-Germany/Stuttgart-Germany.csv")
+test.get_line_chart("data/1614077421_preprocessed/Stuttgart_Germany")
