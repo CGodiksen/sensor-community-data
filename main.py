977f29be3631d22b3010092ae9edc1c149260070
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-01 22:03:10 +0100

    Removed data statistics since it is not relevant to data retrieval and preprocessing.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import logging
 import time
 from datetime import date
 
-from data_statistics import DataStatistics
 from preprocessor import Preprocessor
 from scraper import Scraper
 
@@ -10,12 +9,10 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     path = f"data/{int(time.time())}"
 
-    #scraper = Scraper(["P1", "P2"], start_date=date(2018, 1, 1), end_date=date(2018, 1, 31), sensor_types=["sds011"])
-    #scraper.start()
+    scraper = Scraper(["P1", "P2"], start_date=date(2018, 1, 1), end_date=date(2018, 1, 31), sensor_types=["sds011"])
+    scraper.start()
 
-    #preprocessor = Preprocessor(f"{path}_preprocessed", dataframes=scraper.dataframes, combine_city_data=True,
-    #                            resample_freq="60T", add_lockdown_info=True)
-    #preprocessor.start()
+    preprocessor = Preprocessor(f"{path}_preprocessed", dataframes=scraper.dataframes, combine_city_data=True,
+                                resample_freq="60T", add_lockdown_info=True)
+    preprocessor.start()
 
-    data_statistics = DataStatistics(f"data/1614627910_preprocessed", data_folder="data/1614627910_preprocessed")
-    data_statistics.create_statistics_file()
