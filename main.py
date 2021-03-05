48d806a592c59e6af8e885173f2c8258c9e5e7c6
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 20:54:32 +0100

    Added function to scrape day by day.

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ from datetime import date, timedelta
 from preprocessor import Preprocessor
 from scraper import Scraper
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    path = f"data/{int(time.time())}"
 
-    start_date = date(2017, 1, 1)
-    end_date = date(2017, 1, 3)
+# Scrape the data one day at a time. More crash resilient since data is saved throughout execution.
+def scrape_day_by_day(path, start_date, end_date):
     delta = end_date - start_date
-
+    
     for date in [start_date + timedelta(days=i) for i in range(delta.days + 1)]:
         scraper = Scraper(["P1", "P2"], start_date=date, end_date=date, sensor_types=["sds011"])
         scraper.start()
@@ -20,4 +17,12 @@ if __name__ == '__main__':
         preprocessor = Preprocessor(f"{path}_preprocessed", dataframes=scraper.dataframes, combine_city_data=True,
                                     resample_freq="60T", add_lockdown_info=True)
         preprocessor.start()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    path = f"data/{int(time.time())}"
+
+    start_date = date(2017, 1, 1)
+    end_date = date(2017, 1, 3)
 
