fdebb0373f2d68f3cee900639e0660cdc4df25a3
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 20:40:53 +0100

    Added simplified optimization to main to avoid large refactoring of preprocessor and scraper.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from datetime import date
+from datetime import date, timedelta
 
 from preprocessor import Preprocessor
 from scraper import Scraper
@@ -9,10 +9,15 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     path = f"data/{int(time.time())}"
 
-    scraper = Scraper(["P1", "P2"], start_date=date(2018, 1, 1), end_date=date(2018, 1, 31), sensor_types=["sds011"])
-    scraper.start()
+    start_date = date(2017, 1, 1)
+    end_date = date(2017, 1, 3)
+    delta = end_date - start_date
 
-    preprocessor = Preprocessor(f"{path}_preprocessed", dataframes=scraper.dataframes, combine_city_data=True,
-                                resample_freq="60T", add_lockdown_info=True)
-    preprocessor.start()
+    for date in [start_date + timedelta(days=i) for i in range(delta.days + 1)]:
+        scraper = Scraper(["P1", "P2"], start_date=date, end_date=date, sensor_types=["sds011"])
+        scraper.start()
+
+        preprocessor = Preprocessor(f"{path}_preprocessed", dataframes=scraper.dataframes, combine_city_data=True,
+                                    resample_freq="60T", add_lockdown_info=True)
+        preprocessor.start()
 
