8e323829679844ce9858a6ea0519efb84eb6ad05
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-08 11:37:47 +0100

    Added skeleton for metadata file.

---
+++ metadata.py
@@ -0,0 +1,13 @@
+"""
+Utility functions for gathering metadata about the data from the Sensor.Community archive.
+"""
+
+
+def plot_sensor_location_distribution(sensor_types, date):
+    """Plots a heatmap of how the given sensors are distributed worldwide on the given date."""
+    pass
+
+
+def plot_sensor_count(sensor_types, start_date, end_date):
+    """Plots a line chart showing how many sensors there were each month between the given dates."""
+    pass
