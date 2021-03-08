c1afc69bf1717e32c68029b668a59e7ec30e81bf
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-08 12:49:46 +0100

    Extracting the months between the two given dates.

--- metadata.py
+++ metadata.py
@@ -1,6 +1,11 @@
 """
 Utility functions for gathering metadata about the data from the Sensor.Community archive.
 """
+import matplotlib
+import numpy as np
+from datetime import date, timedelta
+from scraper import Scraper
+
 
 
 def plot_sensor_location_distribution(sensor_types, date):
@@ -10,4 +15,10 @@ def plot_sensor_location_distribution(sensor_types, date):
 
 def plot_sensor_count(sensor_types, start_date, end_date):
     """Plots a line chart showing how many sensors there were each month between the given dates."""
-    pass
+    delta = end_date - start_date
+
+    dates = [start_date + timedelta(days=i) for i in range(delta.days + 1)]
+    dates = list(filter(lambda date: date.day == 1, dates))
+
+
+plot_sensor_count(["sds011"], date(2020, 1, 1), date(2020, 12, 31))
